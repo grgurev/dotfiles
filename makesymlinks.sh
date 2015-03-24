c7255c3455040b7a97ba4a707f1567ded23a6f8c
Marin Grgurev <marin.rgurev@gmail.com>
2015-03-24 14:00:15 +0100

    Initial commit

---
+++ makesymlinks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# .make.sh
+# This scripts creates symlinks from the home directory to any desired dotfiles
+# in ~/.dotfiles
+#
+
+# setting variables
+dir = ~/.dotfiles                                   # dotfiles dirctory
+olddir = ~/.dotfiles-old                            # old dotfiles backup directory
+files = "bashrc vimrc Renviron"     # list of files/folders to symlink in homedir
+
+# create dotfiles-old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# move any existing dotfiles in homedir to .dotfiles-old directory, then create
+# symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/.dotfiles-old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
